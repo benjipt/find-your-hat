5dc52aa5aad0eb08b7ca5fb21c6f0cad9e908f89
[Benji Thompson] <[thompson.benji@gmail.com]>
2021-02-14 20:32:09 -0500

    .print added to Field class

--- main.js
+++ main.js
@@ -9,4 +9,22 @@ class Field {
     constructor(twoDArray) {
         this.field = twoDArray;
     }
+    // Print Method
+    print() {
+        for (let i=0; i<this.field.length; i++) {
+            console.log(this.field[i].join(''));
+        }
+    }
 }
+
+
+// Testing...
+const testField = new Field([
+    ['*', '░', 'O'],
+    ['░', 'O', '░'],
+    ['░', '^', '░'],
+  ]);
+
+// For testing the print method
+//   testField.print();
+// PASSED
